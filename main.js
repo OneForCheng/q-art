043b5ee51384ebb4646e107ddd8747d535bee7a4
oneforcheng <oneforcheng@gmail.com>
2022-01-21 00:04:11 +0800

    feat: sava data to entry file

---
+++ main.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const getEntryFilePath = () => {
+    const entry = process.argv[2] || 'default';
+    return path.resolve(__dirname, `./data/${entry}`);
+}
+
+const saveData = (filePath, data) => {
+    fs.writeFile(filePath, JSON.stringify(data), err => {
+        if (err) {
+            console.error(err)
+        }
+    })
+}
+
+const getNewItem = () => ({
+    title: process.argv[3] || '',
+    content: process.argv[4] || '',
+})
+
+const getFileContent = filePath => {
+    if (fs.existsSync(filePath)) {
+        const origin = fs.readFileSync(filePath, "utf8");
+        return origin ? JSON.parse(origin) : null;
+    }
+    return null;
+}
+
+const main = () => {
+    const filePath = getEntryFilePath();
+    const newItem = getNewItem();
+    const data = getFileContent(filePath) || [];
+    saveData(filePath, [...data, newItem]);
+}
+
+main();
